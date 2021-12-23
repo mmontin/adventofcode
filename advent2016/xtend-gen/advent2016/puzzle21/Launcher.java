0f92281be365eebaccf00b39dd61ab8a44c9db91
mmontin <mathieu.montin@enseeiht.fr>
2021-12-23 02:37:29 +0100

    21 2016

--- advent2016/xtend-gen/advent2016/puzzle21/Launcher.java
+++ advent2016/xtend-gen/advent2016/puzzle21/Launcher.java
@@ -111,6 +111,127 @@ public class Launcher {
       }
     };
     Utils.getInputs(21).forEach(_function);
-    InputOutput.<String>println(sb.toString());
+    InputOutput.<StringBuffer>println(sb);
+    InputOutput.<String>println("--------------");
+    final StringBuffer sb2 = new StringBuffer("fbgdceah");
+    final Consumer<String> _function_1 = new Consumer<String>() {
+      public void accept(final String it) {
+        final String[] split = it.split(" ");
+        String _get = split[0];
+        boolean _matched = false;
+        if (Objects.equal(_get, "swap")) {
+          _matched=true;
+          boolean _equals = (split[1]).equals("position");
+          if (_equals) {
+            final int i = Integer.parseInt(split[2]);
+            final int j = Integer.parseInt(split[5]);
+            final char ch = sb2.charAt(i);
+            sb2.setCharAt(i, sb2.charAt(j));
+            sb2.setCharAt(j, ch);
+          } else {
+            final char ch1 = (split[2]).charAt(0);
+            final char ch2 = (split[5]).charAt(0);
+            int _length = sb2.length();
+            ExclusiveRange _doubleDotLessThan = new ExclusiveRange(0, _length, true);
+            for (final Integer i_1 : _doubleDotLessThan) {
+              boolean _equals_1 = Character.valueOf(sb2.charAt((i_1).intValue())).equals(Character.valueOf(ch1));
+              if (_equals_1) {
+                sb2.setCharAt((i_1).intValue(), ch2);
+              } else {
+                boolean _equals_2 = Character.valueOf(sb2.charAt((i_1).intValue())).equals(Character.valueOf(ch2));
+                if (_equals_2) {
+                  sb2.setCharAt((i_1).intValue(), ch1);
+                }
+              }
+            }
+          }
+        }
+        if (!_matched) {
+          if (Objects.equal(_get, "rotate")) {
+            _matched=true;
+            boolean _equals_3 = (split[1]).equals("based");
+            if (_equals_3) {
+              int _switchResult_1 = (int) 0;
+              int _indexOf = sb2.indexOf(split[6]);
+              switch (_indexOf) {
+                case 1:
+                  _switchResult_1 = 1;
+                  break;
+                case 3:
+                  _switchResult_1 = 2;
+                  break;
+                case 5:
+                  _switchResult_1 = 3;
+                  break;
+                case 7:
+                  _switchResult_1 = 4;
+                  break;
+                case 2:
+                  _switchResult_1 = 6;
+                  break;
+                case 4:
+                  _switchResult_1 = 7;
+                  break;
+                case 6:
+                  _switchResult_1 = 0;
+                  break;
+                default:
+                  _switchResult_1 = 1;
+                  break;
+              }
+              int offset = _switchResult_1;
+              final Consumer<Integer> _function = new Consumer<Integer>() {
+                public void accept(final Integer it) {
+                  final char ch = sb2.charAt(0);
+                  sb2.deleteCharAt(0);
+                  sb2.append(ch);
+                }
+              };
+              new ExclusiveRange(0, offset, true).forEach(_function);
+            } else {
+              boolean _equals_4 = (split[1]).equals("right");
+              final boolean right = (!_equals_4);
+              final int offset_1 = Integer.parseInt(split[2]);
+              final Consumer<Integer> _function_1 = new Consumer<Integer>() {
+                public void accept(final Integer it) {
+                  if (right) {
+                    int _length = sb2.length();
+                    int _minus = (_length - 1);
+                    final char ch = sb2.charAt(_minus);
+                    int _length_1 = sb2.length();
+                    int _minus_1 = (_length_1 - 1);
+                    sb2.deleteCharAt(_minus_1);
+                    sb2.insert(0, ch);
+                  } else {
+                    final char ch_1 = sb2.charAt(0);
+                    sb2.deleteCharAt(0);
+                    sb2.append(ch_1);
+                  }
+                }
+              };
+              new ExclusiveRange(0, offset_1, true).forEach(_function_1);
+            }
+          }
+        }
+        if (!_matched) {
+          if (Objects.equal(_get, "reverse")) {
+            _matched=true;
+            final int i_2 = Integer.parseInt(split[2]);
+            final int j_1 = Integer.parseInt(split[4]);
+            sb2.replace(i_2, (j_1 + 1), IterableExtensions.join(ListExtensions.<Character>reverse(IterableExtensions.<Character>toList(((Iterable<Character>)Conversions.doWrapArray(sb2.substring(i_2, (j_1 + 1)).toCharArray()))))));
+          }
+        }
+        if (!_matched) {
+          {
+            final int i_3 = Integer.parseInt(split[5]);
+            final int j_2 = Integer.parseInt(split[2]);
+            final char ch_1 = sb2.charAt(i_3);
+            sb2.deleteCharAt(i_3).insert(j_2, ch_1);
+          }
+        }
+      }
+    };
+    ListExtensions.<String>reverse(Utils.getInputs(21)).forEach(_function_1);
+    InputOutput.<StringBuffer>println(sb2);
   }
 }
--- advent2016/xtend-src/advent2016/puzzle21/Launcher.xtend
+++ advent2016/xtend-src/advent2016/puzzle21/Launcher.xtend
@@ -69,6 +69,79 @@ class Launcher {
 			}
 		]
 		
-		println(sb.toString)
+		println(sb)
+		println('--------------')
+		
+		val sb2 = new StringBuffer("fbgdceah")
+		
+		Utils.getInputs(21).reverse.forEach[
+			val split = it.split(" ")
+			switch (split.get(0)) {
+				case "swap" : {
+					if (split.get(1).equals("position")) {
+						val i = Integer::parseInt(split.get(2))
+						val j = Integer::parseInt(split.get(5))
+						val ch = sb2.charAt(i)
+						sb2.setCharAt(i,sb2.charAt(j))
+						sb2.setCharAt(j,ch)
+					} else {
+						val ch1 = split.get(2).charAt(0)
+						val ch2 = split.get(5).charAt(0)
+						for (i : 0..<sb2.length) {
+							if (sb2.charAt(i).equals(ch1))
+								sb2.setCharAt(i,ch2)
+							else if (sb2.charAt(i).equals(ch2))
+								sb2.setCharAt(i,ch1)
+						}
+					}
+				}
+				case "rotate" : {
+					if (split.get(1).equals("based")) {
+						var offset = switch(sb2.indexOf(split.get(6))) {
+							case 1 : 1
+							case 3 : 2
+							case 5 : 3
+							case 7 : 4
+							case 2 : 6
+							case 4 : 7
+							case 6 : 0
+							default : 1
+						}
+						(0..<offset).forEach[
+							val ch = sb2.charAt(0)
+							sb2.deleteCharAt(0)
+							sb2.append(ch)
+						]
+					} else {
+						val right = !split.get(1).equals("right")
+						val offset = Integer::parseInt(split.get(2))
+						(0..<offset).forEach[
+							if (right) {
+								val ch = sb2.charAt(sb2.length - 1)
+								sb2.deleteCharAt(sb2.length - 1)
+								sb2.insert(0,ch)
+							} else {
+								val ch = sb2.charAt(0)
+								sb2.deleteCharAt(0)
+								sb2.append(ch)
+							}
+						]
+					}
+				}
+				case "reverse" : {
+					val i = Integer::parseInt(split.get(2))
+					val j = Integer::parseInt(split.get(4))
+					sb2.replace(i,j+1,sb2.substring(i,j+1).toCharArray.toList.reverse.join)
+				}
+				default : {
+					val i = Integer::parseInt(split.get(5))
+					val j = Integer::parseInt(split.get(2))
+					val ch = sb2.charAt(i)
+					sb2.deleteCharAt(i).insert(j,ch)
+				}
+			}
+		]
+		
+		println(sb2)
 	}
 }
