fd27274519b761146ea2aff40466a8a9480903aa
mmontin <mathieu.montin@enseeiht.fr>
2022-12-06 17:13:46 +0100

    improvements

--- advent2022/xtend-gen/advent2022/Day6.java
+++ advent2022/xtend-gen/advent2022/Day6.java
@@ -3,6 +3,8 @@ package advent2022;
 import adventutils.input.InputLoader;
 import java.util.List;
 import org.eclipse.xtext.xbase.lib.Conversions;
+import org.eclipse.xtext.xbase.lib.ExclusiveRange;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.InputOutput;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
 
@@ -11,15 +13,18 @@ public class Day6 {
   private static final List<Character> input = (List<Character>)Conversions.doWrapArray(IterableExtensions.<String>head(new InputLoader(Integer.valueOf(2022), Integer.valueOf(6)).getInputs()).toCharArray());
 
   public static void main(final String[] args) {
-    Day6.compute(4);
-    Day6.compute(14);
+    InputOutput.<Integer>println(Day6.compute(4));
+    InputOutput.<Integer>println(Day6.compute(14));
   }
 
-  public static void compute(final int nb) {
-    int i = nb;
-    while ((IterableExtensions.<Character>toSet(Day6.input.subList((i - nb), i)).size() < nb)) {
-      i++;
-    }
-    InputOutput.<Integer>println(Integer.valueOf(i));
+  public static Integer compute(final int nb) {
+    int _size = Day6.input.size();
+    final Function1<Integer, Boolean> _function = new Function1<Integer, Boolean>() {
+      public Boolean apply(final Integer it) {
+        int _size = IterableExtensions.<Character>toSet(Day6.input.subList(((it).intValue() - nb), (it).intValue())).size();
+        return Boolean.valueOf((_size == nb));
+      }
+    };
+    return IterableExtensions.<Integer>findFirst(new ExclusiveRange(nb, _size, true), _function);
   }
 }
--- advent2022/xtend-src/advent2022/Day6.xtend
+++ advent2022/xtend-src/advent2022/Day6.xtend
@@ -8,14 +8,11 @@ class Day6 {
 	final static List<Character> input = new InputLoader(2022, 6).getInputs.head.toCharArray
 
 	def static void main(String[] args) {
-		compute(4)
-		compute(14)
+		println(compute(4))
+		println(compute(14))
 	}
 
-	def static void compute(int nb) {
-		var i = nb
-		while (input.subList(i - nb, i).toSet.size < nb)
-			i++
-		println(i)
+	def static compute(int nb) {
+		(nb..<input.size).findFirst[input.subList(it-nb,it).toSet.size == nb]
 	}
 }
