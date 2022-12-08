780d69433f773b39c843864a75e0052e49ad0919
mmontin <mathieu.montin@enseeiht.fr>
2022-12-08 01:27:55 +0100

    some optimizations

--- advent2022/xtend-gen/advent2022/Day7.java
+++ advent2022/xtend-gen/advent2022/Day7.java
@@ -10,7 +10,6 @@ import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.Functions.Function2;
 import org.eclipse.xtext.xbase.lib.InputOutput;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
-import org.eclipse.xtext.xbase.lib.ListExtensions;
 
 @SuppressWarnings("all")
 public class Day7 {
@@ -46,8 +45,8 @@ public class Day7 {
   private static final List<Day7.Folder> folders = CollectionLiterals.<Day7.Folder>newArrayList(Day7.root);
 
   public static void main(final String[] args) {
-    final Function1<String, String> _function = new Function1<String, String>() {
-      public String apply(final String it) {
+    final Consumer<String> _function = new Consumer<String>() {
+      public void accept(final String it) {
         String _xifexpression = null;
         boolean _startsWith = it.startsWith("$");
         if (_startsWith) {
@@ -55,79 +54,73 @@ public class Day7 {
         } else {
           _xifexpression = it;
         }
-        return _xifexpression;
-      }
-    };
-    final Function1<String, String[]> _function_1 = new Function1<String, String[]>() {
-      public String[] apply(final String it) {
-        return it.split(" ");
-      }
-    };
-    final Consumer<String[]> _function_2 = new Consumer<String[]>() {
-      public void accept(final String[] it) {
-        String _get = it[0];
+        final String toProcess = _xifexpression;
+        final String[] parts = toProcess.split(" ");
+        String _get = parts[0];
+        final String x = _get;
         boolean _matched = false;
-        if (Objects.equal(_get, "cd")) {
+        if (Objects.equal(x, "cd")) {
           _matched=true;
           Day7.Folder _switchResult_1 = null;
-          String _get_1 = it[1];
+          String _get_1 = parts[1];
+          final String destination = _get_1;
           boolean _matched_1 = false;
-          if (Objects.equal(_get_1, "/")) {
+          if (Objects.equal(destination, "/")) {
             _matched_1=true;
             _switchResult_1 = Day7.root;
           }
           if (!_matched_1) {
-            if (Objects.equal(_get_1, "..")) {
+            if (Objects.equal(destination, "..")) {
               _matched_1=true;
               _switchResult_1 = Day7.current.parent;
             }
           }
           if (!_matched_1) {
-            _switchResult_1 = Day7.current.content.get(it[1]);
+            _switchResult_1 = Day7.current.content.get(destination);
           }
           Day7.current = _switchResult_1;
         }
         if (!_matched) {
-          if (Objects.equal(_get, "ls")) {
+          if (Objects.equal(x, "ls")) {
             _matched=true;
             return;
           }
         }
         if (!_matched) {
-          if (Objects.equal(_get, "dir")) {
+          if (Objects.equal(x, "dir")) {
             _matched=true;
-            String _get_2 = it[1];
+            String _get_2 = parts[1];
             Day7.Folder _folder = new Day7.Folder(_get_2, Day7.current);
             Day7.folders.add(_folder);
           }
         }
         if (!_matched) {
-          Day7.current.addSize(Integer.parseInt(it[0]));
+          Day7.current.addSize(Integer.parseInt(x));
         }
       }
     };
-    ListExtensions.<String, String[]>map(ListExtensions.<String, String>map(new InputLoader(Integer.valueOf(2022), Integer.valueOf(7)).getInputs(), _function), _function_1).forEach(_function_2);
-    final Function1<Day7.Folder, Boolean> _function_3 = new Function1<Day7.Folder, Boolean>() {
+    new InputLoader(Integer.valueOf(2022), Integer.valueOf(7)).getInputs().forEach(_function);
+    final Function1<Day7.Folder, Boolean> _function_1 = new Function1<Day7.Folder, Boolean>() {
       public Boolean apply(final Day7.Folder it) {
         return Boolean.valueOf((it.size <= 100000));
       }
     };
-    final Function2<Integer, Day7.Folder, Integer> _function_4 = new Function2<Integer, Day7.Folder, Integer>() {
+    final Function2<Integer, Day7.Folder, Integer> _function_2 = new Function2<Integer, Day7.Folder, Integer>() {
       public Integer apply(final Integer acc, final Day7.Folder e) {
         return Integer.valueOf(((acc).intValue() + e.size));
       }
     };
-    InputOutput.<Integer>println(IterableExtensions.<Day7.Folder, Integer>fold(IterableExtensions.<Day7.Folder>filter(Day7.folders, _function_3), Integer.valueOf(0), _function_4));
-    final Function1<Day7.Folder, Boolean> _function_5 = new Function1<Day7.Folder, Boolean>() {
+    InputOutput.<Integer>println(IterableExtensions.<Day7.Folder, Integer>fold(IterableExtensions.<Day7.Folder>filter(Day7.folders, _function_1), Integer.valueOf(0), _function_2));
+    final Function1<Day7.Folder, Boolean> _function_3 = new Function1<Day7.Folder, Boolean>() {
       public Boolean apply(final Day7.Folder it) {
         return Boolean.valueOf((it.size >= (Day7.root.size - 40000000)));
       }
     };
-    final Function1<Day7.Folder, Integer> _function_6 = new Function1<Day7.Folder, Integer>() {
+    final Function1<Day7.Folder, Integer> _function_4 = new Function1<Day7.Folder, Integer>() {
       public Integer apply(final Day7.Folder it) {
         return Integer.valueOf(it.size);
       }
     };
-    InputOutput.<Integer>println(Integer.valueOf(IterableExtensions.<Day7.Folder, Integer>minBy(IterableExtensions.<Day7.Folder>filter(Day7.folders, _function_5), _function_6).size));
+    InputOutput.<Integer>println(Integer.valueOf(IterableExtensions.<Day7.Folder, Integer>minBy(IterableExtensions.<Day7.Folder>filter(Day7.folders, _function_3), _function_4).size));
   }
 }
--- advent2022/xtend-src/advent2022/Day7.xtend
+++ advent2022/xtend-src/advent2022/Day7.xtend
@@ -8,42 +8,46 @@ class Day7 {
 
 	// The root of the filesystem
 	static final Folder root = new Folder("root", null)
-	
+
 	// The folder we are currently in
 	static Folder current = root
-	
+
 	// All folders considers, tracked for convenience
 	static final List<Folder> folders = newArrayList(root)
 
 	def static void main(String[] args) {
 
-		new InputLoader(2022, 7).getInputs.map [
-			//removing the "$" as they are of no use
-			if(it.startsWith("$")) it.substring(2) else it
-		].map[split(" ")].forEach [
-			switch (it.get(0)) {
-				// switching to the right place when needed
+		// Treating the input file line by line, updating the file system on the fly
+		new InputLoader(2022, 7).getInputs.forEach [
+			// Ignoring prompts, which are noise
+			val toProcess = it.startsWith("$") ? it.substring(2) : it
+			// Processing the input to split various elements
+			val parts = toProcess.split(" ")
+			// Deciding on what to do based on the first token on the line
+			// Thankfully the grammar is LL(1)
+			switch x : parts.get(0) {
+				// Switching to the right place: the root, the parent or a subdir
 				case "cd":
-					current = switch (it.get(1)) {
+					current = switch destination : parts.get(1) {
 						case "/": root
 						case "..": current.parent
-						default: current.content.get(it.get(1))
+						default: current.content.get(destination)
 					}
-				// ignoring "ls" as this is noise
+				// Ignoring "ls" as this is noise: what follows is part of current
 				case "ls":
 					return
-				// here we create a directory and assign current as parent
+				// Create a directory (and assigning current as parent in the process)
 				case "dir":
-					folders.add(new Folder(it.get(1), current))
-				// when a file is encountered we only have to gather its size
+					folders.add(new Folder(parts.get(1), current))
+				// Gathering the size of a file (the only relevant element)
 				default:
-					current.addSize(Integer.parseInt(it.get(0)))
+					current.addSize(Integer.parseInt(x))
 			}
 		]
 
 		// 1st star requires the sum of all directories of size <= 100000
 		println(folders.filter[it.size <= 100000].fold(0)[acc, e|acc + e.size])
-		
+
 		// 2nd star requires the smallest directory that frees up enough room
 		println(folders.filter[it.size >= root.size - 40000000].minBy[size].size)
 	}
@@ -54,7 +58,7 @@ class Day7 {
 		public int size
 		Map<String, Folder> content
 
-		// When we create a folder, we add it to its parents children
+		// Creating a folder, and adding it to its parent's children
 		new(String _name, Folder _parent) {
 			parent = _parent
 			size = 0
@@ -62,7 +66,7 @@ class Day7 {
 			if(parent !== null) parent.content.put(_name, this)
 		}
 
-		// When adding a file, we update the size of all the ancestors
+		// Updating the size of the folder as well as its ancestors recursively
 		def void addSize(int toAdd) {
 			size += toAdd
 			if(parent !== null) parent.addSize(toAdd)
