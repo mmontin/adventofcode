4cca2ccf2b3d72e8d032db25dadf064d0cf76e04
mmontin <mathieu.montin@tweag.io>
2022-12-14 17:09:46 +0100

    d14 2022

---
+++ advent2022/xtend-gen/advent2022/Day14.java
@@ -0,0 +1,131 @@
+package advent2022;
+
+import adventutils.geometry.Coordinate;
+import adventutils.input.InputLoader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import org.eclipse.xtext.xbase.lib.CollectionLiterals;
+import org.eclipse.xtext.xbase.lib.Conversions;
+import org.eclipse.xtext.xbase.lib.Functions.Function0;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.InputOutput;
+import org.eclipse.xtext.xbase.lib.IntegerRange;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.eclipse.xtext.xbase.lib.ListExtensions;
+
+@SuppressWarnings("all")
+public class Day14 {
+  private static final List<String> inputs = new InputLoader(Integer.valueOf(2022), Integer.valueOf(14)).getInputs();
+
+  private static final Set<Coordinate> rocks = CollectionLiterals.<Coordinate>newHashSet();
+
+  private static final int max_depth = new Function0<Integer>() {
+    public Integer apply() {
+      int _xblockexpression = (int) 0;
+      {
+        Day14.init();
+        final Function1<Coordinate, Integer> _function = new Function1<Coordinate, Integer>() {
+          public Integer apply(final Coordinate it) {
+            return Integer.valueOf(it.getY());
+          }
+        };
+        _xblockexpression = IterableExtensions.<Coordinate, Integer>maxBy(Day14.rocks, _function).getY();
+      }
+      return _xblockexpression;
+    }
+  }.apply().intValue();
+
+  private static final Coordinate source = new Coordinate(500, 0);
+
+  public static void init() {
+    Day14.rocks.clear();
+    final Consumer<String> _function = new Consumer<String>() {
+      public void accept(final String line) {
+        final Function1<String, Coordinate> _function = new Function1<String, Coordinate>() {
+          public Coordinate apply(final String it) {
+            return new Coordinate(it);
+          }
+        };
+        final List<Coordinate> coords = ListExtensions.<String, Coordinate>map(((List<String>)Conversions.doWrapArray(line.split(" -> "))), _function);
+        int _size = coords.size();
+        int _minus = (_size - 2);
+        final Consumer<Integer> _function_1 = new Consumer<Integer>() {
+          public void accept(final Integer it) {
+            Day14.rocks.addAll(Coordinate.getAllCoordsOnLine(coords.get((it).intValue()), coords.get(((it).intValue() + 1))));
+          }
+        };
+        new IntegerRange(0, _minus).forEach(_function_1);
+      }
+    };
+    Day14.inputs.forEach(_function);
+  }
+
+  public static void main(final String[] args) {
+    int i = 0;
+    while (Day14.flow()) {
+      i++;
+    }
+    InputOutput.<Integer>println(Integer.valueOf(i));
+    Day14.init();
+    i = 0;
+    while ((!Day14.rocks.contains(Day14.source))) {
+      {
+        Day14.flow_with_floor();
+        i++;
+      }
+    }
+    InputOutput.<Integer>print(Integer.valueOf(i));
+  }
+
+  public static boolean flow() {
+    boolean _xblockexpression = false;
+    {
+      Coordinate current = Day14.source;
+      do {
+        {
+          final Coordinate down = current.move(Coordinate.Direction.UP);
+          final ArrayList<Coordinate> possibilities = CollectionLiterals.<Coordinate>newArrayList(down, down.move(Coordinate.Direction.LEFT), down.move(Coordinate.Direction.RIGHT));
+          possibilities.removeAll(Day14.rocks);
+          int _size = possibilities.size();
+          boolean _equals = (_size == 0);
+          if (_equals) {
+            Day14.rocks.add(current);
+            return true;
+          } else {
+            current = possibilities.get(0);
+          }
+        }
+      } while((current.getY() < Day14.max_depth));
+      _xblockexpression = false;
+    }
+    return _xblockexpression;
+  }
+
+  public static void flow_with_floor() {
+    Coordinate current = Day14.source;
+    do {
+      {
+        final Coordinate down = current.move(Coordinate.Direction.UP);
+        final ArrayList<Coordinate> possibilities = CollectionLiterals.<Coordinate>newArrayList(down, down.move(Coordinate.Direction.LEFT), down.move(Coordinate.Direction.RIGHT));
+        possibilities.removeAll(Day14.rocks);
+        final Predicate<Coordinate> _function = new Predicate<Coordinate>() {
+          public boolean test(final Coordinate it) {
+            int _y = it.getY();
+            return (_y > (Day14.max_depth + 1));
+          }
+        };
+        possibilities.removeIf(_function);
+        int _size = possibilities.size();
+        boolean _equals = (_size == 0);
+        if (_equals) {
+          Day14.rocks.add(current);
+        } else {
+          current = possibilities.get(0);
+        }
+      }
+    } while((!Day14.rocks.contains(current)));
+  }
+}
---
+++ advent2022/xtend-src/advent2022/Day14.xtend
@@ -0,0 +1,71 @@
+package advent2022
+
+import adventutils.geometry.Coordinate
+import adventutils.geometry.Coordinate.Direction
+import adventutils.input.InputLoader
+import java.util.Set
+import java.util.List
+
+class Day14 {
+
+	static final List<String> inputs = new InputLoader(2022, 14).inputs
+	static final Set<Coordinate> rocks = newHashSet
+	static final int max_depth = {
+		init
+		rocks.maxBy[y].y
+	}
+	static final Coordinate source = new Coordinate(500, 0)
+
+	def static init() {
+		rocks.clear
+		inputs.forEach [ line |
+			val coords = line.split(" -> ").map[new Coordinate(it)]
+			(0 .. coords.size - 2).forEach [
+				rocks.addAll(Coordinate.getAllCoordsOnLine(coords.get(it), coords.get(it + 1)))
+			]
+		]
+	}
+
+	def static void main(String[] args) {
+		var i = 0
+		while(flow) i ++
+		println(i)
+		
+		init
+		i = 0
+		while(!rocks.contains(source)) {
+			flow_with_floor
+			i ++
+		}
+		print(i)
+	}
+	
+	def static boolean flow() {
+		var current = source
+		do {
+			val down = current.move(Direction.UP)
+			val possibilities = newArrayList(down,down.move(Direction.LEFT),down.move(Direction.RIGHT))
+			possibilities.removeAll(rocks)
+			if (possibilities.size == 0) {
+				rocks.add(current)
+				return true
+			} else
+				current = possibilities.get(0)
+		} while (current.y < max_depth)
+		false
+	}
+	
+	def static flow_with_floor() {
+		var current = source
+		do {
+			val down = current.move(Direction.UP)
+			val possibilities = newArrayList(down,down.move(Direction.LEFT),down.move(Direction.RIGHT))
+			possibilities.removeAll(rocks)
+			possibilities.removeIf[it.y > max_depth+1]
+			if (possibilities.size == 0)
+				rocks.add(current)
+			else
+				current = possibilities.get(0)
+		} while (!rocks.contains(current))
+	}
+}
