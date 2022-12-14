a3e5bb8f79b01c1d8afb707cb2ddc85fdaeab7f6
mmontin <mathieu.montin@enseeiht.fr>
2022-12-15 00:52:42 +0100

    2017 d10

--- advent2017/xtend-gen/advent2017/Day10.java
+++ advent2017/xtend-gen/advent2017/Day10.java
@@ -1,13 +1,13 @@
 package advent2017;
 
 import adventutils.input.InputLoader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Consumer;
 import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 import org.eclipse.xtext.xbase.lib.Conversions;
 import org.eclipse.xtext.xbase.lib.ExclusiveRange;
-import org.eclipse.xtext.xbase.lib.Functions.Function0;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.InputOutput;
 import org.eclipse.xtext.xbase.lib.IntegerRange;
@@ -46,30 +46,33 @@ public class Day10 {
     }
   }
 
-  private static final int LIST_SIZE = 4;
+  private static final int LIST_SIZE = 256;
 
-  private static Day10.Link current_link = new Function0<Day10.Link>() {
-    public Day10.Link apply() {
-      Day10.Link _xblockexpression = null;
-      {
-        Day10.Link chain_head = new Day10.Link(0, null);
-        Day10.Link current = chain_head;
-        IntegerRange _upTo = new IntegerRange(1, Day10.LIST_SIZE);
-        for (final Integer i : _upTo) {
-          Day10.Link _link = new Day10.Link((i).intValue(), current);
-          current = _link;
-        }
-        current.updateNext(chain_head);
-        _xblockexpression = chain_head;
+  private static Day10.Link current_link = null;
+
+  public static Day10.Link init() {
+    Day10.Link _xblockexpression = null;
+    {
+      Day10.Link chain_head = new Day10.Link(0, null);
+      Day10.Link current = chain_head;
+      ExclusiveRange _doubleDotLessThan = new ExclusiveRange(1, Day10.LIST_SIZE, true);
+      for (final Integer i : _doubleDotLessThan) {
+        Day10.Link _link = new Day10.Link((i).intValue(), current);
+        current = _link;
       }
-      return _xblockexpression;
+      current.updateNext(chain_head);
+      _xblockexpression = Day10.current_link = chain_head;
     }
-  }.apply();
+    return _xblockexpression;
+  }
 
   private static int skip_size = 0;
 
+  private static int moved = 0;
+
   public static void main(final String[] args) {
-    Day10.print();
+    String input = IterableExtensions.<String>head(new InputLoader(Integer.valueOf(2017), Integer.valueOf(10)).getInputs());
+    Day10.init();
     final Function1<String, Integer> _function = new Function1<String, Integer>() {
       public Integer apply(final String it) {
         return Integer.valueOf(Integer.parseInt(it));
@@ -78,18 +81,76 @@ public class Day10 {
     final Consumer<Integer> _function_1 = new Consumer<Integer>() {
       public void accept(final Integer it) {
         Day10.reverse((it).intValue());
+        int _moved = Day10.moved;
+        Day10.moved = (_moved + (((it).intValue() + Day10.skip_size) % Day10.LIST_SIZE));
         Day10.skip();
-        Day10.print();
       }
     };
-    ListExtensions.<String, Integer>map(((List<String>)Conversions.doWrapArray(IterableExtensions.<String>head(new InputLoader(Integer.valueOf(2017), Integer.valueOf(10)).getInputs()).split(","))), _function).forEach(_function_1);
-    InputOutput.<Integer>println(Integer.valueOf((Day10.current_link.value * Day10.current_link.next.value)));
+    ListExtensions.<String, Integer>map(((List<String>)Conversions.doWrapArray(input.split(","))), _function).forEach(_function_1);
+    Day10.Link toPrint = Day10.current_link;
+    ExclusiveRange _doubleDotLessThan = new ExclusiveRange(0, Day10.moved, true);
+    for (final Integer i : _doubleDotLessThan) {
+      toPrint = toPrint.previous;
+    }
+    InputOutput.<Integer>println(Integer.valueOf((toPrint.value * toPrint.next.value)));
+    Day10.init();
+    final Function1<Character, Integer> _function_2 = new Function1<Character, Integer>() {
+      public Integer apply(final Character it) {
+        return Integer.valueOf(((int) (it).charValue()));
+      }
+    };
+    List<Integer> _map = ListExtensions.<Character, Integer>map(((List<Character>)Conversions.doWrapArray(input.toCharArray())), _function_2);
+    final ArrayList<Integer> second_pass = new ArrayList<Integer>(_map);
+    second_pass.addAll(CollectionLiterals.<Integer>newArrayList(Integer.valueOf(17), Integer.valueOf(31), Integer.valueOf(73), Integer.valueOf(47), Integer.valueOf(23)));
+    Day10.skip_size = 0;
+    Day10.moved = 0;
+    final Consumer<Integer> _function_3 = new Consumer<Integer>() {
+      public void accept(final Integer it) {
+        final Consumer<Integer> _function = new Consumer<Integer>() {
+          public void accept(final Integer it_1) {
+            Day10.reverse((it_1).intValue());
+            int _moved = Day10.moved;
+            Day10.moved = (_moved + (((it_1).intValue() + Day10.skip_size) % Day10.LIST_SIZE));
+            Day10.skip();
+          }
+        };
+        second_pass.forEach(_function);
+      }
+    };
+    new ExclusiveRange(0, 64, true).forEach(_function_3);
+    toPrint = Day10.current_link;
+    ExclusiveRange _doubleDotLessThan_1 = new ExclusiveRange(0, Day10.moved, true);
+    for (final Integer i_1 : _doubleDotLessThan_1) {
+      toPrint = toPrint.previous;
+    }
+    final ArrayList<Integer> numbers = CollectionLiterals.<Integer>newArrayList();
+    IntegerRange _upTo = new IntegerRange(0, 15);
+    for (final Integer i_2 : _upTo) {
+      {
+        int result = toPrint.value;
+        IntegerRange _upTo_1 = new IntegerRange(0, 14);
+        for (final Integer j : _upTo_1) {
+          {
+            toPrint = toPrint.next;
+            result = (result ^ toPrint.value);
+          }
+        }
+        toPrint = toPrint.next;
+        numbers.add(Integer.valueOf(result));
+      }
+    }
+    final Function1<Integer, String> _function_4 = new Function1<Integer, String>() {
+      public String apply(final Integer it) {
+        return Integer.toHexString((it).intValue());
+      }
+    };
+    InputOutput.<String>print(IterableExtensions.join(ListExtensions.<Integer, String>map(numbers, _function_4), ""));
   }
 
   public static void print() {
     Day10.Link current = Day10.current_link;
-    IntegerRange _upTo = new IntegerRange(0, Day10.LIST_SIZE);
-    for (final Integer i : _upTo) {
+    ExclusiveRange _doubleDotLessThan = new ExclusiveRange(0, Day10.LIST_SIZE, true);
+    for (final Integer i : _doubleDotLessThan) {
       {
         String _plus = (Integer.valueOf(current.value) + ",");
         InputOutput.<String>print(_plus);
@@ -100,35 +161,51 @@ public class Day10 {
   }
 
   public static Day10.Link reverse(final int quantity) {
-    Day10.Link _xifexpression = null;
-    if ((quantity < Day10.LIST_SIZE)) {
-      Day10.Link _xblockexpression = null;
-      {
-        final LinkedList<Day10.Link> links = CollectionLiterals.<Day10.Link>newLinkedList();
-        Day10.Link my_current = Day10.current_link;
-        ExclusiveRange _doubleDotLessThan = new ExclusiveRange(0, quantity, true);
-        for (final Integer i : _doubleDotLessThan) {
-          {
-            links.addFirst(my_current);
-            my_current = my_current.next;
-          }
+    Day10.Link _xblockexpression = null;
+    {
+      final LinkedList<Day10.Link> links = CollectionLiterals.<Day10.Link>newLinkedList();
+      Day10.Link my_current = Day10.current_link;
+      ExclusiveRange _doubleDotLessThan = new ExclusiveRange(0, quantity, true);
+      for (final Integer i : _doubleDotLessThan) {
+        {
+          links.addFirst(my_current);
+          my_current = my_current.next;
         }
-        Day10.Link before = Day10.current_link.previous;
-        ExclusiveRange _doubleDotLessThan_1 = new ExclusiveRange(0, quantity, true);
-        for (final Integer i_1 : _doubleDotLessThan_1) {
-          {
-            before.updateNext(links.get((i_1).intValue()));
-            before = before.next;
-          }
-        }
-        before.updateNext(my_current);
-        _xblockexpression = Day10.current_link = my_current;
       }
-      _xifexpression = _xblockexpression;
-    } else {
-      _xifexpression = null;
+      Day10.Link _xifexpression = null;
+      if ((quantity < Day10.LIST_SIZE)) {
+        Day10.Link _xblockexpression_1 = null;
+        {
+          Day10.Link before = Day10.current_link.previous;
+          ExclusiveRange _doubleDotLessThan_1 = new ExclusiveRange(0, quantity, true);
+          for (final Integer i_1 : _doubleDotLessThan_1) {
+            {
+              before.updateNext(links.get((i_1).intValue()));
+              before = before.next;
+            }
+          }
+          before.updateNext(my_current);
+          _xblockexpression_1 = Day10.current_link = my_current;
+        }
+        _xifexpression = _xblockexpression_1;
+      } else {
+        Day10.Link _xblockexpression_2 = null;
+        {
+          final Consumer<Day10.Link> _function = new Consumer<Day10.Link>() {
+            public void accept(final Day10.Link it) {
+              Day10.Link tmp = it.previous;
+              it.previous = it.next;
+              it.next = tmp;
+            }
+          };
+          links.forEach(_function);
+          _xblockexpression_2 = Day10.current_link = IterableExtensions.<Day10.Link>head(links);
+        }
+        _xifexpression = _xblockexpression_2;
+      }
+      _xblockexpression = _xifexpression;
     }
-    return _xifexpression;
+    return _xblockexpression;
   }
 
   public static void skip() {
@@ -138,6 +215,6 @@ public class Day10 {
       }
     };
     new ExclusiveRange(0, Day10.skip_size, true).forEach(_function);
-    Day10.skip_size++;
+    Day10.skip_size = ((Day10.skip_size + 1) % Day10.LIST_SIZE);
   }
 }
--- advent2017/xtend-src/advent2017/Day10.xtend
+++ advent2017/xtend-src/advent2017/Day10.xtend
@@ -1,35 +1,72 @@
 package advent2017
 
 import adventutils.input.InputLoader
+import java.util.ArrayList
 
 class Day10 {
 
-	static final int LIST_SIZE = 4
+	static final int LIST_SIZE = 256
 
-	static Link current_link = {
+	static Link current_link = null 
+	
+	def static init() {
 		var chain_head = new Link(0, null)
 		var current = chain_head;
-		for (i : 1 .. LIST_SIZE)
+		for (i : 1 ..< LIST_SIZE)
 			current = new Link(i, current)
 		current.updateNext(chain_head)
-		chain_head
+		current_link = chain_head
 	}
 
 	static int skip_size = 0
+	static int moved = 0
 
 	def static void main(String[] args) {
-		print
-		new InputLoader(2017, 10).inputs.head.split(",").map[Integer.parseInt(it)].forEach [
+		
+		var input = new InputLoader(2017,10).inputs.head
+		
+		init
+		input.split(",").map[Integer.parseInt(it)].forEach [
 			reverse(it)
+			moved += (it + skip_size)% LIST_SIZE
 			skip
-			print
 		]
-		println(current_link.value * current_link.next.value)
+		var toPrint = current_link
+		for (i : 0 ..< moved)
+			toPrint = toPrint.previous
+		println(toPrint.value * toPrint.next.value)
+		
+		init
+		val second_pass = new ArrayList(input.toCharArray.map[it as int])
+		second_pass.addAll(newArrayList(17,31,73,47,23))
+		skip_size = 0
+		moved = 0
+		(0..<64).forEach[
+			second_pass.forEach[
+				reverse(it)
+				moved += (it + skip_size)% LIST_SIZE
+				skip
+			]
+		]
+		toPrint = current_link
+		for (i : 0 ..< moved)
+			toPrint = toPrint.previous
+		val numbers = newArrayList ;
+		for (i : 0..15) {
+			var result = toPrint.value
+			for (j : 0..14) {
+				toPrint = toPrint.next
+				result = result.bitwiseXor(toPrint.value)
+			}
+			toPrint = toPrint.next
+			numbers.add(result)
+		}
+		print(numbers.map[Integer.toHexString(it)].join(""))
 	}
 
 	def static print() {
 		var current = current_link
-		for (i : 0 .. LIST_SIZE) {
+		for (i : 0 ..< LIST_SIZE) {
 			print(current.value + ",")
 			current = current.next
 		}
@@ -37,13 +74,13 @@ class Day10 {
 	}
 
 	def static reverse(int quantity) {
+		val links = newLinkedList
+		var my_current = current_link;
+		for (i : 0 ..< quantity) {
+			links.addFirst(my_current)
+			my_current = my_current.next
+		}
 		if (quantity < LIST_SIZE) {
-			val links = newLinkedList
-			var my_current = current_link;
-			for (i : 0 ..< quantity) {
-				links.addFirst(my_current)
-				my_current = my_current.next
-			}
 			var before = current_link.previous
 			for (i : 0 ..< quantity) {
 				before.updateNext(links.get(i))
@@ -52,13 +89,18 @@ class Day10 {
 			before.updateNext(my_current)
 			current_link = my_current
 		} else {
-			
+			links.forEach [
+				var tmp = it.previous
+				it.previous = it.next
+				it.next = tmp
+			]
+			current_link = links.head
 		}
 	}
 
 	def static void skip() {
 		(0 ..< skip_size).forEach[current_link = current_link.next]
-		skip_size++
+		skip_size = (skip_size + 1) % LIST_SIZE
 	}
 
 	static class Link {
