4ff31d2d1165e1b1d01503492e005a271e5d62b3
mmontin <mathieu.montin@enseeiht.fr>
2022-12-20 19:04:14 +0100

    D19 part 1

---
+++ advent2022/xtend-gen/advent2022/Day20.java
@@ -0,0 +1,7 @@
+package advent2022;
+
+@SuppressWarnings("all")
+public class Day20 {
+  public static void main(final String[] args) {
+  }
+}
---
+++ advent2022/xtend-src/advent2022/Day20.xtend
@@ -0,0 +1,7 @@
+package advent2022
+
+class Day20 {
+	def static void main(String[] args) {
+		
+	}
+}
