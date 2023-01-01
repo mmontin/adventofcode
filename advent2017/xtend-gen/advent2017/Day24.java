7d03d33ab48571aee5b33e479e9230a06edcd4f9
mmontin <mathieu.montin@enseeiht.fr>
2023-01-01 19:05:33 +0100

    2017 d24

--- advent2017/xtend-gen/advent2017/Day24.java
+++ advent2017/xtend-gen/advent2017/Day24.java
@@ -12,13 +12,75 @@ import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.Functions.Function2;
 import org.eclipse.xtext.xbase.lib.InputOutput;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
-import org.eclipse.xtext.xbase.lib.ListExtensions;
 import org.eclipse.xtext.xbase.lib.Pair;
 
 @SuppressWarnings("all")
 public class Day24 {
+  public static class Element {
+    private int left;
+
+    private int right;
+
+    public Element(final int _left, final int _right) {
+      this.left = _left;
+      this.right = _right;
+    }
+
+    public int weight() {
+      return (this.left + this.right);
+    }
+
+    public boolean connectsWithPort(final int port) {
+      return ((this.left == port) || (this.right == port));
+    }
+
+    public boolean connectsWithOther(final Day24.Element other) {
+      return (this.connectsWithPort(other.left) || this.connectsWithPort(other.right));
+    }
+
+    public boolean accessibleFrom(final Set<Day24.Element> others) {
+      final Function1<Day24.Element, Boolean> _function = new Function1<Day24.Element, Boolean>() {
+        public Boolean apply(final Day24.Element it) {
+          return Boolean.valueOf(it.connectsWithOther(Element.this));
+        }
+      };
+      return IterableExtensions.<Day24.Element>exists(others, _function);
+    }
+
+    public int otherPort(final int port) {
+      int _xifexpression = (int) 0;
+      if ((this.left == port)) {
+        _xifexpression = this.right;
+      } else {
+        _xifexpression = this.left;
+      }
+      return _xifexpression;
+    }
+
+    public int hashCode() {
+      return (this.left * this.right);
+    }
+
+    public boolean equals(final Object other) {
+      boolean _switchResult = false;
+      boolean _matched = false;
+      if (other instanceof Day24.Element) {
+        _matched=true;
+        _switchResult = ((((Day24.Element)other).left == this.left) && (((Day24.Element)other).right == this.right));
+      }
+      if (!_matched) {
+        _switchResult = false;
+      }
+      return _switchResult;
+    }
+
+    public String toString() {
+      return (((("[ " + Integer.valueOf(this.left)) + " , ") + Integer.valueOf(this.right)) + " ]");
+    }
+  }
+
   public static class Bridge implements State {
-    private final Set<Pair<Integer, Integer>> remaining;
+    private final Set<Day24.Element> remaining;
 
     private final int last_plug;
 
@@ -31,54 +93,71 @@ public class Day24 {
     private final int hashCode;
 
     public Bridge() {
-      this(new HashSet<Pair<Integer, Integer>>(Day24.elements), 0);
+      this(new HashSet<Day24.Element>(Day24.elements), 0);
     }
 
-    public Bridge(final Set<Pair<Integer, Integer>> _remaining, final int _last_plug) {
+    public Bridge(final Set<Day24.Element> _remaining, final int _last_plug) {
       this.remaining = _remaining;
       this.last_plug = _last_plug;
       this.neighbours = CollectionLiterals.<Pair<State, Integer>>newArrayList();
-      final Function1<Pair<Integer, Integer>, Boolean> _function = new Function1<Pair<Integer, Integer>, Boolean>() {
-        public Boolean apply(final Pair<Integer, Integer> it) {
-          return Boolean.valueOf((((it.getKey()).intValue() == Bridge.this.last_plug) || ((it.getValue()).intValue() == Bridge.this.last_plug)));
+      final Function1<Day24.Element, Boolean> _function = new Function1<Day24.Element, Boolean>() {
+        public Boolean apply(final Day24.Element it) {
+          return Boolean.valueOf(it.connectsWithPort(Bridge.this.last_plug));
         }
       };
-      final Consumer<Pair<Integer, Integer>> _function_1 = new Consumer<Pair<Integer, Integer>>() {
-        public void accept(final Pair<Integer, Integer> it) {
-          HashSet<Pair<Integer, Integer>> _xblockexpression = null;
+      final Consumer<Day24.Element> _function_1 = new Consumer<Day24.Element>() {
+        public void accept(final Day24.Element it) {
+          HashSet<Day24.Element> _xblockexpression = null;
           {
-            final HashSet<Pair<Integer, Integer>> output = new HashSet<Pair<Integer, Integer>>(Bridge.this.remaining);
+            final HashSet<Day24.Element> output = new HashSet<Day24.Element>(Bridge.this.remaining);
             output.remove(it);
             _xblockexpression = output;
           }
-          Integer _xifexpression = null;
-          Integer _key = it.getKey();
-          boolean _equals = ((_key).intValue() == Bridge.this.last_plug);
-          if (_equals) {
-            _xifexpression = it.getValue();
-          } else {
-            _xifexpression = it.getKey();
-          }
-          Day24.Bridge _bridge = new Day24.Bridge(_xblockexpression, (_xifexpression).intValue());
-          Integer _key_1 = it.getKey();
-          Integer _value = it.getValue();
-          int _plus = ((_key_1).intValue() + (_value).intValue());
-          int _minus = (Day24.MAX_ELEMNT_VALUE - _plus);
-          Pair<State, Integer> _mappedTo = Pair.<State, Integer>of(((State) _bridge), Integer.valueOf(_minus));
+          int _otherPort = it.otherPort(Bridge.this.last_plug);
+          Day24.Bridge _bridge = new Day24.Bridge(_xblockexpression, _otherPort);
+          Pair<State, Integer> _mappedTo = Pair.<State, Integer>of(((State) _bridge), Integer.valueOf(0));
           Bridge.this.neighbours.add(_mappedTo);
         }
       };
-      IterableExtensions.<Pair<Integer, Integer>>filter(this.remaining, _function).forEach(_function_1);
+      IterableExtensions.<Day24.Element>filter(this.remaining, _function).forEach(_function_1);
       this.isGoal = this.neighbours.isEmpty();
-      final Function2<Integer, Pair<Integer, Integer>, Integer> _function_2 = new Function2<Integer, Pair<Integer, Integer>, Integer>() {
-        public Integer apply(final Integer acc, final Pair<Integer, Integer> v) {
-          Integer _key = v.getKey();
-          Integer _value = v.getValue();
-          int _plus = ((_key).intValue() + (_value).intValue());
-          return Integer.valueOf((((acc).intValue() + Day24.MAX_ELEMNT_VALUE) - _plus));
+      Day24.Element _element = new Day24.Element(this.last_plug, this.last_plug);
+      final HashSet<Day24.Element> accessible = CollectionLiterals.<Day24.Element>newHashSet(_element);
+      final HashSet<Day24.Element> to_try = new HashSet<Day24.Element>(this.remaining);
+      final Function1<Day24.Element, Boolean> _function_2 = new Function1<Day24.Element, Boolean>() {
+        public Boolean apply(final Day24.Element it) {
+          return Boolean.valueOf(it.accessibleFrom(accessible));
         }
       };
-      this.minToGoal = (IterableExtensions.<Pair<Integer, Integer>, Integer>fold(this.remaining, Integer.valueOf(0), _function_2)).intValue();
+      Day24.Element next_accessible = IterableExtensions.<Day24.Element>findFirst(to_try, _function_2);
+      while ((next_accessible != null)) {
+        {
+          accessible.add(next_accessible);
+          to_try.remove(next_accessible);
+          final Function1<Day24.Element, Boolean> _function_3 = new Function1<Day24.Element, Boolean>() {
+            public Boolean apply(final Day24.Element it) {
+              return Boolean.valueOf(it.accessibleFrom(accessible));
+            }
+          };
+          next_accessible = IterableExtensions.<Day24.Element>findFirst(to_try, _function_3);
+        }
+      }
+      final Function2<Integer, Day24.Element, Integer> _function_3 = new Function2<Integer, Day24.Element, Integer>() {
+        public Integer apply(final Integer acc, final Day24.Element v) {
+          int _weight = v.weight();
+          return Integer.valueOf(((acc).intValue() + _weight));
+        }
+      };
+      Integer _fold = IterableExtensions.<Day24.Element, Integer>fold(to_try, Integer.valueOf(0), _function_3);
+      int _xifexpression = (int) 0;
+      if (Day24.round1) {
+        _xifexpression = 0;
+      } else {
+        int _size = to_try.size();
+        _xifexpression = (_size * 1000);
+      }
+      int _plus = ((_fold).intValue() + _xifexpression);
+      this.minToGoal = _plus;
       this.hashCode = this.remaining.hashCode();
     }
 
@@ -117,52 +196,55 @@ public class Day24 {
     }
   }
 
-  private static final List<Pair<Integer, Integer>> elements = new InputLoader(Integer.valueOf(2017), Integer.valueOf(24)).<Pair<Integer, Integer>>getInputs(new Function1<String, Pair<Integer, Integer>>() {
-    public Pair<Integer, Integer> apply(final String it) {
-      Pair<Integer, Integer> _xblockexpression = null;
+  private static final List<Day24.Element> elements = new InputLoader(Integer.valueOf(2017), Integer.valueOf(24)).<Day24.Element>getInputs(new Function1<String, Day24.Element>() {
+    public Day24.Element apply(final String it) {
+      Day24.Element _xblockexpression = null;
       {
         final String[] split = it.split("/");
         int _parseInt = Integer.parseInt(split[0]);
         int _parseInt_1 = Integer.parseInt(split[1]);
-        _xblockexpression = Pair.<Integer, Integer>of(Integer.valueOf(_parseInt), Integer.valueOf(_parseInt_1));
+        _xblockexpression = new Day24.Element(_parseInt, _parseInt_1);
       }
       return _xblockexpression;
     }
   });
 
-  private static final int MAX_ELEMNT_VALUE = (int) IterableExtensions.<Integer>max(ListExtensions.<Pair<Integer, Integer>, Integer>map(Day24.elements, new Function1<Pair<Integer, Integer>, Integer>() {
-    public Integer apply(final Pair<Integer, Integer> it) {
-      Integer _value = it.getValue();
-      Integer _key = it.getKey();
-      return Integer.valueOf(((_value).intValue() + (_key).intValue()));
-    }
-  }));
+  private static boolean round1 = true;
 
   public static void main(final String[] args) {
-    InputOutput.<Integer>println(Integer.valueOf(Day24.elements.size()));
     Day24.Bridge _bridge = new Day24.Bridge();
     final AStar computation = new AStar(_bridge).run();
-    final Consumer<State> _function = new Consumer<State>() {
-      public void accept(final State it) {
-        InputOutput.<State>println(it);
-      }
-    };
-    computation.minPath().forEach(_function);
-    final Function1<Pair<Integer, Integer>, Boolean> _function_1 = new Function1<Pair<Integer, Integer>, Boolean>() {
-      public Boolean apply(final Pair<Integer, Integer> it) {
+    final Function1<Day24.Element, Boolean> _function = new Function1<Day24.Element, Boolean>() {
+      public Boolean apply(final Day24.Element it) {
         State _last = IterableExtensions.<State>last(computation.minPath());
         boolean _contains = ((Day24.Bridge) _last).remaining.contains(it);
         return Boolean.valueOf((!_contains));
       }
     };
-    final Function2<Integer, Pair<Integer, Integer>, Integer> _function_2 = new Function2<Integer, Pair<Integer, Integer>, Integer>() {
-      public Integer apply(final Integer acc, final Pair<Integer, Integer> v) {
-        Integer _key = v.getKey();
-        int _plus = ((acc).intValue() + (_key).intValue());
-        Integer _value = v.getValue();
-        return Integer.valueOf((_plus + (_value).intValue()));
+    final Function2<Integer, Day24.Element, Integer> _function_1 = new Function2<Integer, Day24.Element, Integer>() {
+      public Integer apply(final Integer acc, final Day24.Element v) {
+        int _weight = v.weight();
+        return Integer.valueOf(((acc).intValue() + _weight));
       }
     };
-    InputOutput.<Integer>println(IterableExtensions.<Pair<Integer, Integer>, Integer>fold(IterableExtensions.<Pair<Integer, Integer>>filter(Day24.elements, _function_1), Integer.valueOf(0), _function_2));
+    InputOutput.<Integer>println(IterableExtensions.<Day24.Element, Integer>fold(IterableExtensions.<Day24.Element>filter(Day24.elements, _function), Integer.valueOf(0), _function_1));
+    Day24.round1 = false;
+    Day24.Bridge _bridge_1 = new Day24.Bridge();
+    computation.initialize(_bridge_1);
+    computation.run();
+    final Function1<Day24.Element, Boolean> _function_2 = new Function1<Day24.Element, Boolean>() {
+      public Boolean apply(final Day24.Element it) {
+        State _last = IterableExtensions.<State>last(computation.minPath());
+        boolean _contains = ((Day24.Bridge) _last).remaining.contains(it);
+        return Boolean.valueOf((!_contains));
+      }
+    };
+    final Function2<Integer, Day24.Element, Integer> _function_3 = new Function2<Integer, Day24.Element, Integer>() {
+      public Integer apply(final Integer acc, final Day24.Element v) {
+        int _weight = v.weight();
+        return Integer.valueOf(((acc).intValue() + _weight));
+      }
+    };
+    InputOutput.<Integer>println(IterableExtensions.<Day24.Element, Integer>fold(IterableExtensions.<Day24.Element>filter(Day24.elements, _function_2), Integer.valueOf(0), _function_3));
   }
 }
--- advent2017/xtend-src/advent2017/Day24.xtend
+++ advent2017/xtend-src/advent2017/Day24.xtend
@@ -9,25 +9,76 @@ import adventutils.astar.AStar
 
 class Day24 {
 
-	static final List<Pair<Integer, Integer>> elements = new InputLoader(2017, 24).getInputs [
+	static final List<Element> elements = new InputLoader(2017, 24).getInputs [
 		val split = it.split("/")
-		Integer.parseInt(split.get(0)) -> Integer.parseInt(split.get(1))
+		new Element(Integer.parseInt(split.get(0)), Integer.parseInt(split.get(1)))
 	]
 
-	static final int MAX_ELEMNT_VALUE = elements.map[it.value + it.key].max
+	static boolean round1 = true
 
 	def static void main(String[] args) {
-		println(elements.size)
 		val computation = new AStar(new Bridge).run
-		computation.minPath.forEach[println(it)]
 		println(elements.filter[!(computation.minPath.last as Bridge).remaining.contains(it)].fold(0) [ acc, v |
-			acc + v.key + v.value
+			acc + v.weight
 		])
+
+		round1 = false
+		computation.initialize(new Bridge)
+		computation.run
+		println(elements.filter[!(computation.minPath.last as Bridge).remaining.contains(it)].fold(0) [ acc, v |
+			acc + v.weight
+		])
+	}
+
+	static class Element {
+
+		int left
+		int right
+
+		new(int _left, int _right) {
+			left = _left
+			right = _right
+		}
+
+		def weight() {
+			left + right
+		}
+
+		def connectsWithPort(int port) {
+			left == port || right == port
+		}
+
+		def connectsWithOther(Element other) {
+			connectsWithPort(other.left) || connectsWithPort(other.right)
+		}
+
+		def accessibleFrom(Set<Element> others) {
+			others.exists[it.connectsWithOther(this)]
+		}
+
+		def otherPort(int port) {
+			left == port ? right : left
+		}
+
+		override hashCode() {
+			left * right
+		}
+
+		override equals(Object other) {
+			switch other {
+				Element: other.left == left && other.right == right
+				default: false
+			}
+		}
+
+		override toString() {
+			"[ " + left + " , " + right + " ]"
+		}
 	}
 
 	static class Bridge implements State {
 
-		final Set<Pair<Integer, Integer>> remaining
+		final Set<Element> remaining
 		final int last_plug
 
 		final List<Pair<State, Integer>> neighbours
@@ -40,27 +91,35 @@ class Day24 {
 			this(new HashSet(elements), 0)
 		}
 
-		new(Set<Pair<Integer, Integer>> _remaining, int _last_plug) {
+		new(Set<Element> _remaining, int _last_plug) {
 
 			remaining = _remaining
 			last_plug = _last_plug
 
 			neighbours = newArrayList
-			remaining.filter[it.key == last_plug || it.value == last_plug].forEach [
+			remaining.filter[connectsWithPort(last_plug)].forEach [
 				neighbours.add(new Bridge(
 					{
 						val output = new HashSet(remaining)
 						output.remove(it)
 						output
 					},
-					it.key == last_plug ? it.value : it.key
-				) as State -> MAX_ELEMNT_VALUE - (it.key + it.value))
+					it.otherPort(last_plug)
+				) as State -> 0)
 			]
 
 			isGoal = neighbours.isEmpty
-			minToGoal = remaining.fold(0) [ acc, v |
-				acc + MAX_ELEMNT_VALUE - (v.key + v.value)
-			]
+
+			val accessible = newHashSet(new Element(last_plug, last_plug))
+			val to_try = new HashSet(remaining)
+			var next_accessible = to_try.findFirst[it.accessibleFrom(accessible)]
+			while (next_accessible !== null) {
+				accessible.add(next_accessible)
+				to_try.remove(next_accessible)
+				next_accessible = to_try.findFirst[it.accessibleFrom(accessible)]
+			}
+
+			minToGoal = to_try.fold(0)[acc, v|acc + v.weight] + (round1 ? 0 : to_try.size * 1000)
 
 			hashCode = remaining.hashCode
 		}
