051fd75c66dfd79dbcf876ed0f7782f02c392c66
mmontin <mathieu.montin@moduscreate.com>
2024-12-24 00:35:36 +0100

    d21 attempt

--- advent2024/xtend-gen/advent2024/Day21.java
+++ advent2024/xtend-gen/advent2024/Day21.java
@@ -10,6 +10,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 import org.eclipse.xtext.xbase.lib.Conversions;
@@ -23,9 +25,17 @@ import org.eclipse.xtext.xbase.lib.ListExtensions;
 
 @SuppressWarnings("all")
 public class Day21 {
-  private static final List<String> example = CollectionLiterals.<String>newArrayList("0", "2", "9", "A");
+  private static final List<String> example = CollectionLiterals.<String>newArrayList("A", "0", "2", "9", "A");
 
-  private static final List<ArrayList<String>> inputs = ListExtensions.<String, ArrayList<String>>map(CollectionLiterals.<String>newArrayList("129A", "176A", "169A", "805A", "208A"), ((Function1<String, ArrayList<String>>) (String it) -> {
+  private static final List<ArrayList<String>> examples = ListExtensions.<String, ArrayList<String>>map(CollectionLiterals.<String>newArrayList("A029A", "A980A", "A179A", "A456A", "A379A"), ((Function1<String, ArrayList<String>>) (String it) -> {
+    final Function1<Character, String> _function = (Character it_1) -> {
+      return (it_1 + "");
+    };
+    List<String> _map = ListExtensions.<Character, String>map(((List<Character>)Conversions.doWrapArray(it.toCharArray())), _function);
+    return new ArrayList<String>(_map);
+  }));
+
+  private static final List<ArrayList<String>> inputs = ListExtensions.<String, ArrayList<String>>map(CollectionLiterals.<String>newArrayList("A129A", "A176A", "A169A", "A805A", "A208A"), ((Function1<String, ArrayList<String>>) (String it) -> {
     final Function1<Character, String> _function = (Character it_1) -> {
       return (it_1 + "");
     };
@@ -70,7 +80,7 @@ public class Day21 {
 
   private static final Coordinate forbidden_pad_numeric = new Coordinate(3, 0);
 
-  private static final Map<Coordinate, String> pad_directionnel_map = new Function0<Map<Coordinate, String>>() {
+  private static final Map<Coordinate, String> pad_directional_map = new Function0<Map<Coordinate, String>>() {
     @Override
     public Map<Coordinate, String> apply() {
       HashMap<Coordinate, String> _xblockexpression = null;
@@ -82,20 +92,20 @@ public class Day21 {
           };
           return ListExtensions.<Character, String>map(((List<Character>)Conversions.doWrapArray(it.toCharArray())), _function_1);
         };
-        final List<List<String>> pad_directionnel = ListExtensions.<String, List<String>>map(Collections.<String>unmodifiableList(CollectionLiterals.<String>newArrayList(" ^A", "<v>")), _function);
-        int _size = pad_directionnel.size();
+        final List<List<String>> pad_directional = ListExtensions.<String, List<String>>map(Collections.<String>unmodifiableList(CollectionLiterals.<String>newArrayList(" ^A", "<v>")), _function);
+        int _size = pad_directional.size();
         int _minus = (_size - 1);
         IntegerRange _upTo = new IntegerRange(0, _minus);
         for (final Integer i : _upTo) {
-          int _size_1 = pad_directionnel.get(0).size();
+          int _size_1 = pad_directional.get(0).size();
           int _minus_1 = (_size_1 - 1);
           IntegerRange _upTo_1 = new IntegerRange(0, _minus_1);
           for (final Integer j : _upTo_1) {
-            String _get = pad_directionnel.get((i).intValue()).get((j).intValue());
+            String _get = pad_directional.get((i).intValue()).get((j).intValue());
             boolean _notEquals = (!Objects.equals(_get, " "));
             if (_notEquals) {
               Coordinate _coordinate = new Coordinate((i).intValue(), (j).intValue());
-              res.put(_coordinate, pad_directionnel.get((i).intValue()).get((j).intValue()));
+              res.put(_coordinate, pad_directional.get((i).intValue()).get((j).intValue()));
             }
           }
         }
@@ -105,85 +115,168 @@ public class Day21 {
     }
   }.apply();
 
-  private static final Coordinate forbidden_pad_directionnel = new Coordinate(0, 0);
+  private static final Set<Coordinate> pad_keys = Day21.pad_directional_map.keySet();
+
+  private static final Coordinate forbidden_pad_directional = new Coordinate(0, 0);
+
+  private static final Set<String> chunks = new Function0<Set<String>>() {
+    @Override
+    public Set<String> apply() {
+      HashSet<String> _xblockexpression = null;
+      {
+        final HashSet<String> res = CollectionLiterals.<String>newHashSet();
+        for (final Coordinate c1 : Day21.pad_keys) {
+          for (final Coordinate c2 : Day21.pad_keys) {
+            {
+              String _get = Day21.pad_directional_map.get(c1);
+              String _plus = (_get + "");
+              String _get_1 = Day21.pad_directional_map.get(c2);
+              String _plus_1 = (_get_1 + "");
+              final Function1<ArrayList<ArrayList<String>>, String> _function = (ArrayList<ArrayList<String>> it) -> {
+                return IterableExtensions.join(IterableExtensions.<ArrayList<String>>head(it));
+              };
+              final String x = IterableExtensions.<String>head(IterableExtensions.<ArrayList<ArrayList<String>>, String>map(Day21.treat_code(
+                CollectionLiterals.<String>newArrayList(_plus, _plus_1), 
+                false), _function));
+              res.add(x);
+            }
+          }
+        }
+        _xblockexpression = res;
+      }
+      return _xblockexpression;
+    }
+  }.apply();
 
   public static void main(final String[] args) {
-    final Function2<Integer, ArrayList<String>, Integer> _function = (Integer acc, ArrayList<String> el) -> {
-      int _xblockexpression = (int) 0;
-      {
-        final int number = Integer.parseInt(IterableExtensions.join(el.subList(0, 3)));
-        int _treat_layered_code = Day21.treat_layered_code(el);
-        int _multiply = (number * _treat_layered_code);
-        _xblockexpression = ((acc).intValue() + _multiply);
-      }
-      return Integer.valueOf(_xblockexpression);
+    final Function2<Long, ArrayList<String>, Long> _function = (Long acc, ArrayList<String> el) -> {
+      Long _compute = Day21.compute(el, 1);
+      int _size = el.size();
+      int _minus = (_size - 1);
+      int _parseInt = Integer.parseInt(IterableExtensions.join(el.subList(1, _minus)));
+      long _multiply = ((_compute).longValue() * _parseInt);
+      return Long.valueOf(((acc).longValue() + _multiply));
     };
-    InputOutput.<Integer>println(IterableExtensions.<ArrayList<String>, Integer>fold(Day21.inputs, Integer.valueOf(0), _function));
+    InputOutput.<Long>println(IterableExtensions.<ArrayList<String>, Long>fold(Day21.inputs, Long.valueOf(0L), _function));
+    final Function2<Long, ArrayList<String>, Long> _function_1 = (Long acc, ArrayList<String> el) -> {
+      Long _compute = Day21.compute(el, 24);
+      int _size = el.size();
+      int _minus = (_size - 1);
+      int _parseInt = Integer.parseInt(IterableExtensions.join(el.subList(1, _minus)));
+      long _multiply = ((_compute).longValue() * _parseInt);
+      return Long.valueOf(((acc).longValue() + _multiply));
+    };
+    InputOutput.<Long>println(IterableExtensions.<ArrayList<String>, Long>fold(Day21.inputs, Long.valueOf(0L), _function_1));
   }
 
-  public static int treat_layered_code(final List<String> code) {
-    int _xblockexpression = (int) 0;
+  public static Long compute(final List<String> code, final int occurrences) {
+    Long _xblockexpression = null;
     {
-      final Iterable<ArrayList<ArrayList<String>>> first_layers_inputs = Day21.treat_code(code, true);
-      final ArrayList<ArrayList<String>> first_layers_inputs_merged = Day21.merge_possibilities(first_layers_inputs);
-      final Function1<ArrayList<String>, Iterable<ArrayList<ArrayList<String>>>> _function = (ArrayList<String> it) -> {
-        return Day21.treat_code(it, false);
+      final Function2<ArrayList<ArrayList<String>>, ArrayList<ArrayList<String>>, ArrayList<ArrayList<String>>> _function = (ArrayList<ArrayList<String>> acc, ArrayList<ArrayList<String>> el) -> {
+        ArrayList<ArrayList<String>> _xblockexpression_1 = null;
+        {
+          final ArrayList<ArrayList<String>> res = CollectionLiterals.<ArrayList<String>>newArrayList();
+          final Consumer<ArrayList<String>> _function_1 = (ArrayList<String> ac) -> {
+            final Consumer<ArrayList<String>> _function_2 = (ArrayList<String> e) -> {
+              final ArrayList<String> tmp = CollectionLiterals.<String>newArrayList();
+              tmp.addAll(ac);
+              tmp.addAll(e);
+              res.add(tmp);
+            };
+            el.forEach(_function_2);
+          };
+          acc.forEach(_function_1);
+          _xblockexpression_1 = res;
+        }
+        return _xblockexpression_1;
       };
-      List<Iterable<ArrayList<ArrayList<String>>>> second_layer = ListExtensions.<ArrayList<String>, Iterable<ArrayList<ArrayList<String>>>>map(first_layers_inputs_merged, _function);
-      final ArrayList<ArrayList<String>> all_second_layer_merged = CollectionLiterals.<ArrayList<String>>newArrayList();
-      final Consumer<Iterable<ArrayList<ArrayList<String>>>> _function_1 = (Iterable<ArrayList<ArrayList<String>>> it) -> {
-        all_second_layer_merged.addAll(Day21.merge_possibilities(it));
+      final ArrayList<ArrayList<String>> first_layer_codes = IterableExtensions.<ArrayList<ArrayList<String>>, ArrayList<ArrayList<String>>>fold(Day21.treat_code(code, true), CollectionLiterals.<ArrayList<String>>newArrayList(CollectionLiterals.<String>newArrayList("A")), _function);
+      final Function1<ArrayList<String>, Long> _function_1 = (ArrayList<String> first_layer_code) -> {
+        Long _xblockexpression_1 = null;
+        {
+          final Function1<ArrayList<ArrayList<String>>, ArrayList<String>> _function_2 = (ArrayList<ArrayList<String>> it) -> {
+            final Function1<ArrayList<String>, Boolean> _function_3 = (ArrayList<String> it_1) -> {
+              return Boolean.valueOf(Day21.chunks.contains(IterableExtensions.join(it_1)));
+            };
+            return IterableExtensions.<ArrayList<String>>findFirst(it, _function_3);
+          };
+          final Function1<ArrayList<String>, String> _function_3 = (ArrayList<String> it) -> {
+            return IterableExtensions.join(it);
+          };
+          final Iterable<String> first_layer_treated = IterableExtensions.<ArrayList<String>, String>map(IterableExtensions.<ArrayList<ArrayList<String>>, ArrayList<String>>map(Day21.treat_code(first_layer_code, false), _function_2), _function_3);
+          final Function2<HashMap<String, Long>, String, HashMap<String, Long>> _function_4 = (HashMap<String, Long> acc, String el) -> {
+            HashMap<String, Long> _xblockexpression_2 = null;
+            {
+              final BiFunction<Long, Long, Long> _function_5 = (Long x, Long y) -> {
+                return Long.valueOf(((x).longValue() + (y).longValue()));
+              };
+              acc.merge(el, Long.valueOf(1L), _function_5);
+              _xblockexpression_2 = acc;
+            }
+            return _xblockexpression_2;
+          };
+          HashMap<String, Long> layer_map = IterableExtensions.<String, HashMap<String, Long>>fold(first_layer_treated, CollectionLiterals.<String, Long>newHashMap(), _function_4);
+          IntegerRange _upTo = new IntegerRange(1, occurrences);
+          for (final Integer i : _upTo) {
+            layer_map = Day21.transform_chunk(layer_map);
+          }
+          final Function2<Long, Map.Entry<String, Long>, Long> _function_5 = (Long acc, Map.Entry<String, Long> el) -> {
+            int _length = el.getKey().length();
+            Long _value = el.getValue();
+            long _multiply = (_length * (_value).longValue());
+            return Long.valueOf(((acc).longValue() + _multiply));
+          };
+          _xblockexpression_1 = IterableExtensions.<Map.Entry<String, Long>, Long>fold(layer_map.entrySet(), Long.valueOf(0L), _function_5);
+        }
+        return _xblockexpression_1;
       };
-      second_layer.forEach(_function_1);
-      final Function1<ArrayList<String>, Iterable<ArrayList<ArrayList<String>>>> _function_2 = (ArrayList<String> it) -> {
-        return Day21.treat_code(it, false);
-      };
-      final List<Iterable<ArrayList<ArrayList<String>>>> third_layer = ListExtensions.<ArrayList<String>, Iterable<ArrayList<ArrayList<String>>>>map(all_second_layer_merged, _function_2);
-      final ArrayList<ArrayList<String>> all_third_layer_merged = CollectionLiterals.<ArrayList<String>>newArrayList();
-      final Consumer<Iterable<ArrayList<ArrayList<String>>>> _function_3 = (Iterable<ArrayList<ArrayList<String>>> it) -> {
-        all_third_layer_merged.addAll(Day21.merge_possibilities(it));
-      };
-      third_layer.forEach(_function_3);
-      final Function1<ArrayList<String>, Integer> _function_4 = (ArrayList<String> it) -> {
-        return Integer.valueOf(it.size());
-      };
-      _xblockexpression = IterableExtensions.<ArrayList<String>, Integer>minBy(all_third_layer_merged, _function_4).size();
+      _xblockexpression = IterableExtensions.<Long>min(ListExtensions.<ArrayList<String>, Long>map(first_layer_codes, _function_1));
     }
     return _xblockexpression;
   }
 
-  public static ArrayList<ArrayList<String>> merge_possibilities(final Iterable<ArrayList<ArrayList<String>>> input) {
-    final Function2<ArrayList<ArrayList<String>>, ArrayList<ArrayList<String>>, ArrayList<ArrayList<String>>> _function = (ArrayList<ArrayList<String>> acc, ArrayList<ArrayList<String>> el) -> {
-      ArrayList<ArrayList<String>> _xblockexpression = null;
+  public static HashMap<String, Long> transform_chunk(final Map<String, Long> layer_map) {
+    final Function2<HashMap<String, Long>, Map.Entry<String, Long>, HashMap<String, Long>> _function = (HashMap<String, Long> acc, Map.Entry<String, Long> el) -> {
+      HashMap<String, Long> _xblockexpression = null;
       {
-        final ArrayList<ArrayList<String>> res = CollectionLiterals.<ArrayList<String>>newArrayList();
-        final Consumer<ArrayList<String>> _function_1 = (ArrayList<String> l1) -> {
-          final Consumer<ArrayList<String>> _function_2 = (ArrayList<String> l2) -> {
-            final ArrayList<String> current = CollectionLiterals.<String>newArrayList();
-            current.addAll(l2);
-            current.addAll(l1);
-            res.add(current);
-          };
-          acc.forEach(_function_2);
+        final Function1<Character, String> _function_1 = (Character it) -> {
+          return (it + "");
         };
-        el.forEach(_function_1);
-        _xblockexpression = res;
+        List<String> _map = ListExtensions.<Character, String>map(((List<Character>)Conversions.doWrapArray(el.getKey().toCharArray())), _function_1);
+        final ArrayList<String> code = new ArrayList<String>(_map);
+        code.add(0, "A");
+        final Function1<ArrayList<ArrayList<String>>, ArrayList<String>> _function_2 = (ArrayList<ArrayList<String>> it) -> {
+          final Function1<ArrayList<String>, Boolean> _function_3 = (ArrayList<String> it_1) -> {
+            return Boolean.valueOf(Day21.chunks.contains(IterableExtensions.join(it_1)));
+          };
+          return IterableExtensions.<ArrayList<String>>findFirst(it, _function_3);
+        };
+        final Function1<ArrayList<String>, String> _function_3 = (ArrayList<String> it) -> {
+          return IterableExtensions.join(it);
+        };
+        final Consumer<String> _function_4 = (String it) -> {
+          final BiFunction<Long, Long, Long> _function_5 = (Long x, Long y) -> {
+            return Long.valueOf(((x).longValue() + (y).longValue()));
+          };
+          acc.merge(it, el.getValue(), _function_5);
+        };
+        IterableExtensions.<ArrayList<String>, String>map(IterableExtensions.<ArrayList<ArrayList<String>>, ArrayList<String>>map(Day21.treat_code(code, false), _function_2), _function_3).forEach(_function_4);
+        _xblockexpression = acc;
       }
       return _xblockexpression;
     };
-    return IterableExtensions.<ArrayList<ArrayList<String>>, ArrayList<ArrayList<String>>>fold(input, CollectionLiterals.<ArrayList<String>>newArrayList(CollectionLiterals.<String>newArrayList()), _function);
+    return IterableExtensions.<Map.Entry<String, Long>, HashMap<String, Long>>fold(layer_map.entrySet(), CollectionLiterals.<String, Long>newHashMap(), _function);
   }
 
   public static Iterable<ArrayList<ArrayList<String>>> treat_code(final List<String> code, final boolean is_numeric) {
     Iterable<ArrayList<ArrayList<String>>> _xblockexpression = null;
     {
-      code.add(0, "A");
       final Function1<String, Coordinate> _function = (String c) -> {
         Map<Coordinate, String> _xifexpression = null;
         if (is_numeric) {
           _xifexpression = Day21.pad_numeric_map;
         } else {
-          _xifexpression = Day21.pad_directionnel_map;
+          _xifexpression = Day21.pad_directional_map;
         }
         final Function1<Map.Entry<Coordinate, String>, Boolean> _function_1 = (Map.Entry<Coordinate, String> it) -> {
           String _value = it.getValue();
@@ -203,7 +296,7 @@ public class Day21 {
           if (is_numeric) {
             _xifexpression = Day21.forbidden_pad_numeric;
           } else {
-            _xifexpression = Day21.forbidden_pad_directionnel;
+            _xifexpression = Day21.forbidden_pad_directional;
           }
           final Iterable<List<String>> paths = Day21.replay_and_trim(_treat_pair, _get, _xifexpression);
           final ArrayList<ArrayList<String>> ans = CollectionLiterals.<ArrayList<String>>newArrayList();
--- advent2024/xtend-src/advent2024/Day21.xtend
+++ advent2024/xtend-src/advent2024/Day21.xtend
@@ -7,6 +7,7 @@ import java.util.ArrayList
 import java.util.HashSet
 import java.util.List
 import java.util.Map
+import java.util.Set
 
 class Day21 {
 
@@ -25,9 +26,13 @@ class Day21 {
 //+---+---+---+
 //| < | v | > |
 //+---+---+---+
-
-	static final List<String> example = newArrayList("0", "2", "9", "A")
-	static final List<ArrayList<String>> inputs = newArrayList("129A", "176A", "169A", "805A", "208A").map[new ArrayList(toCharArray.map[it + ""])]
+	static final List<String> example = newArrayList("A", "0", "2", "9", "A")
+	static final List<ArrayList<String>> examples = newArrayList("A029A", "A980A", "A179A", "A456A", "A379A").map [
+		new ArrayList(toCharArray.map[it + ""])
+	]
+	static final List<ArrayList<String>> inputs = newArrayList("A129A", "A176A", "A169A", "A805A", "A208A").map [
+		new ArrayList(toCharArray.map[it + ""])
+	]
 
 	static final Map<Coordinate, String> pad_numeric_map = {
 		val res = newHashMap
@@ -40,63 +45,95 @@ class Day21 {
 	}
 	static final Coordinate forbidden_pad_numeric = new Coordinate(3, 0)
 
-	static final Map<Coordinate, String> pad_directionnel_map = {
+	static final Map<Coordinate, String> pad_directional_map = {
 		val res = newHashMap
-		val pad_directionnel = #[" ^A", "<v>"].map[toCharArray.map[it + ""]]
-		for (i : 0 .. pad_directionnel.size - 1)
-			for (j : 0 .. pad_directionnel.get(0).size - 1)
-				if (pad_directionnel.get(i).get(j) != " ")
-					res.put(new Coordinate(i, j), pad_directionnel.get(i).get(j))
+		val pad_directional = #[" ^A", "<v>"].map[toCharArray.map[it + ""]]
+		for (i : 0 .. pad_directional.size - 1)
+			for (j : 0 .. pad_directional.get(0).size - 1)
+				if (pad_directional.get(i).get(j) != " ")
+					res.put(new Coordinate(i, j), pad_directional.get(i).get(j))
 		res
 	}
-	static final Coordinate forbidden_pad_directionnel = new Coordinate(0, 0)
+	static final Set<Coordinate> pad_keys = pad_directional_map.keySet
+
+	static final Coordinate forbidden_pad_directional = new Coordinate(0, 0)
+
+	static final Set<String> chunks = {
+		val res = newHashSet
+		for (c1 : pad_keys) {
+			for (c2 : pad_keys) {
+				val x = treat_code(
+					newArrayList(
+						pad_directional_map.get(c1) + "",
+						pad_directional_map.get(c2) + ""
+					),
+					false
+				).map[it.head.join].head
+				res.add(x)
+			}
+		}
+		res
+	}
 
 	def static void main(String[] args) {
-
-		println(inputs.fold(0) [acc,el |
-			val number = Integer.parseInt(el.subList(0,3).join)
-			acc + number * el.treat_layered_code
+		println(inputs.fold(0L) [ acc, el |
+			acc + compute(el, 1) * Integer.parseInt(el.subList(1, el.size - 1).join)
+		])
+		
+		println(inputs.fold(0L) [ acc, el |
+			acc + compute(el, 24) * Integer.parseInt(el.subList(1, el.size - 1).join)
 		])
 	}
 
-	def static treat_layered_code(List<String> code) {
-		val first_layers_inputs = code.treat_code(true)
-		val first_layers_inputs_merged = first_layers_inputs.merge_possibilities
-		
-		var second_layer = first_layers_inputs_merged.map[treat_code(false)]
-		val all_second_layer_merged = newArrayList
-		second_layer.forEach[all_second_layer_merged.addAll(it.merge_possibilities)]
+	def static compute(List<String> code, int occurrences) {
 
-		val third_layer = all_second_layer_merged.map[treat_code(false)]
-		val all_third_layer_merged = newArrayList
-		third_layer.forEach[all_third_layer_merged.addAll(it.merge_possibilities)]
-
-		all_third_layer_merged.minBy[size].size
-	}
-
-	def static ArrayList<ArrayList<String>> merge_possibilities(Iterable<ArrayList<ArrayList<String>>> input) {
-		input.fold(newArrayList(newArrayList)) [ ArrayList<ArrayList<String>> acc, el |
+		val first_layer_codes = code.treat_code(true).fold(newArrayList(newArrayList("A"))) [ acc, el |
 			val res = newArrayList
-			el.forEach [ l1 |
-				acc.forEach [ l2 |
-					val current = newArrayList
-					current.addAll(l2)
-					current.addAll(l1)
-					res.add(current)
+			acc.forEach [ ac |
+				el.forEach [ e |
+					val tmp = newArrayList
+					tmp.addAll(ac)
+					tmp.addAll(e)
+					res.add(tmp)
 				]
 			]
 			res
+		]
+
+		first_layer_codes.map [ first_layer_code |
+
+			val first_layer_treated = first_layer_code.treat_code(false).map [
+				it.findFirst[chunks.contains(it.join)]
+			].map[join]
+
+			var layer_map = first_layer_treated.fold(newHashMap)[acc, el|acc.merge(el, 1L)[x, y|x + y]; acc]
+
+			for (i : 1 .. occurrences)
+				layer_map = transform_chunk(layer_map)
+
+			layer_map.entrySet.fold(0L) [ acc, el |
+				acc + el.key.length * el.value
+			]
+		].min
+	}
+
+	def static transform_chunk(Map<String, Long> layer_map) {
+		layer_map.entrySet.fold(newHashMap) [ acc, el |
+			val code = new ArrayList(el.key.toCharArray.map[it + ""])
+			code.add(0, "A")
+			code.treat_code(false).map[it.findFirst[chunks.contains(it.join)]].map[join].forEach [
+				acc.merge(it, el.value)[x, y|x + y]
+			]
+			acc
 		]
 	}
 
 	// Takes a code to be typed by a robot on a given pad (decided by is_numeric)
 	// And gives back all possibles codes to type on a directional pad to obtain this code
 	def static treat_code(List<String> code, boolean is_numeric) {
-		// whenever we want to type a code, we always begin from letter "A"
-		code.add(0, "A")
 		// We store the coordinates where the various letters to be signed sit 
 		val coordinates = code.map [ c |
-			(is_numeric ? pad_numeric_map : pad_directionnel_map).entrySet.findFirst [
+			(is_numeric ? pad_numeric_map : pad_directional_map).entrySet.findFirst [
 				it.value == c
 			].key
 		]
@@ -106,7 +143,7 @@ class Day21 {
 		(0 .. coordinates.size - 2).map [
 			val paths = treat_pair(coordinates.get(it), coordinates.get(it + 1)).replay_and_trim(
 				coordinates.get(it),
-				is_numeric ? forbidden_pad_numeric : forbidden_pad_directionnel
+				is_numeric ? forbidden_pad_numeric : forbidden_pad_directional
 			)
 			val ans = newArrayList
 			paths.forEach [
@@ -141,7 +178,7 @@ class Day21 {
 		res.add(poss1)
 		res.add(poss2)
 		res
-		//ListUtils.intertwine(x_moves, y_moves)
+	// ListUtils.intertwine(x_moves, y_moves)
 	}
 
 	// Takes a set of possibles paths, replay them from a given source coordinate, 
